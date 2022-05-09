5728e2f5e0fc46532fa19639d0a81a6e73645a31
EIDOSDATA <ihjisan00@gmail.com>
2022-05-10 08:56:24 +0900

    upload

---
+++ NRF52840/led_pwm/build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD nrf52840dk_nrf52840 CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT c:/Users/eidos/Desktop/NRF52840/led_pwm;C:/Users/eidos/ncs/v1.9.1/nrf;C:/Users/eidos/ncs/v1.9.1/zephyr CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE C:/Users/eidos/Desktop/NRF52840/led_pwm/prj.conf CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_OPTIMIZATIONS y CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_THREAD_INFO y CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON c:\Users\eidos\ncs\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
